32c130a1fcac0f7af55999e2c3031bd85a116d49
Ethan Han <ethanhan@Ethans-MacBook-Pro-2.local>
2025-09-24 17:14:42 +1000

    sdlkfj

--- src/pages/front_page/front_page.jsx
+++ src/pages/front_page/front_page.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Timeline from './components';
 
 export default () => {
   return (
-    <div>random div here</div>
+    <>
+      <div>random div here</div>
+      <Timeline />
+    </>
   )
 }
