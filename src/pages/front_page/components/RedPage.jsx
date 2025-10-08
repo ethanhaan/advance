6106299dc4d4af194e21819ebb25b488eff953f7
adsi3525 <adsi3525@uni.sydney.edu.au>
2025-10-08 21:13:58 +1100

    checkpoint: before parallex scroll implementation

---
+++ src/pages/front_page/components/RedPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './RedPage.css';
+
+export default function RedPage() {
+  return (
+    <section className="red-page-section">
+      {/* You can add content here later */}
+      Testing Testing
+    </section>
+  );
+}
