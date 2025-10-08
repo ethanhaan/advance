d1fb3700f55f8ac4b824f6a1d2c9baa4077175dd
adsi3525 <adsi3525@uni.sydney.edu.au>
2025-10-08 22:06:30 +1100

    parallex scroll implemented

--- src/pages/front_page/front_page.jsx
+++ src/pages/front_page/front_page.jsx
@@ -3,12 +3,14 @@ import Timeline from './components/timeline';
 import CircularCarousel from './components/circular-carousel';
 import Hero from './components/hero';
 import Navbar from '../../global_components/Navbar/Navbar';
+import RedPage from './components/RedPage';
 
 export default function frontpage () {
   return (
     <div>
       <Navbar />
       <Hero />
+      <RedPage />
       <CircularCarousel />
       <Timeline />
     </div>
