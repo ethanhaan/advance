88bdd67fdcdb2d6906e8396051ff4021d90fad52
adsi3525 <adsi3525@uni.sydney.edu.au>
2025-10-08 22:13:25 +1100

    parallex scroll implemented

--- src/pages/front_page/front_page.jsx
+++ src/pages/front_page/front_page.jsx
@@ -4,13 +4,17 @@ import CircularCarousel from './components/circular-carousel';
 import Hero from './components/hero';
 import Navbar from '../../global_components/Navbar/Navbar';
 import RedPage from './components/RedPage';
+import './front_page.css';
 
 export default function frontpage () {
   return (
     <div>
       <Navbar />
-      <Hero />
-      <RedPage />
+      {/* Wrap Hero and RedPage in a container to scope the sticky effect */}
+      <div className="hero-sticky-container">
+        <Hero />
+        <RedPage />
+      </div>
       <CircularCarousel />
       <Timeline />
     </div>
