95621bc4c6421029a02d8953c3ecf74e254bcac1
Ethan Han <ethanhan@ethans-mbp-2.staff.wireless.sydney.edu.au>
2025-10-22 20:49:44 +1100

    sldkjf

--- src/pages/workshops_page/workshops_page.jsx
+++ src/pages/workshops_page/workshops_page.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import Hero from "./components/hero.jsx";
 import WorkshopsCarousel from "./components/workshops_carousel.jsx";
 import WorkshopContent from "./components/workshop_content.jsx";
+import Offerings from "./components/offerings.jsx";
 
 import img1 from "./assets/canterbury_workshop.png";
 import img2 from "./assets/canterbury_workshop.png";
@@ -16,6 +17,9 @@ export default function WorkshopsPage({ setIsNavbarFixed }) {
   return (
     <>
       <Hero />
+      <div style={{position: "absolute", width: "100vw", transform: "translateY(-140px)"}}>
+        <Offerings />
+      </div>
       <WorkshopsCarousel
         images={[img1, img2, img3]}
         aspect={16 / 9} // change if you prefer 4/3 etc.
